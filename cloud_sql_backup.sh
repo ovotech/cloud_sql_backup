49beee4a9c414a6510990b2bc1d9c6047ea2934b
eversC <chris.every@ovoenergy.com>
2019-09-18 14:45:41 +0100

    Add licencing

--- cloud_sql_backup.sh
+++ cloud_sql_backup.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+# Copyright 2019 OVO Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -e
 
 [ -z "$DB_VERSION" ] && echo "DB_VERSION is required" && invalid=true
@@ -32,7 +47,7 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 echo_out "Grabbing details of the latest GCP backup to create sql backup from"
 BACKUP_DATA=$(gcloud sql backups list \
-  --instance $SOURCE_BACKUP_INSTANCE \
+  --instance "$SOURCE_BACKUP_INSTANCE" \
   --filter STATUS=SUCCESSFUL \
   --limit 1 | sed 1,1d | tr -s ' ')
 BACKUP_ID=$(echo "$BACKUP_DATA" | cut -d ' ' -f 1)
